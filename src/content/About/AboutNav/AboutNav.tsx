a5c6e5c17987a0685eb3685b10873cf20fa375e1
razianmm <raziandev@gmail.com>
2022-12-26 23:08:20 -0500

    Added working navigation to About page

--- src/content/About/AboutNav/AboutNav.tsx
+++ src/content/About/AboutNav/AboutNav.tsx
@@ -1,9 +1,15 @@
 import "./AboutNav.scss"
 
-export const AboutNav = () => {
+export const AboutNav = ({
+  updateDisplay,
+}: {
+  updateDisplay: React.Dispatch<React.SetStateAction<string>>
+}) => {
   return (
     <ul className="about__links">
-      <li>Design</li>
+      <li>
+        <button onClick={() => updateDisplay("Design")}>Design</button>
+      </li>
       <li>Accessibility</li>
       <li>Tooling</li>
       <li>Testing</li>
--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -1,14 +1,19 @@
+import { useState } from "react"
 import { SiteSection } from "../../components"
-import { AboutNav, AboutIntro } from "../../content/About"
+import { AboutNav, AboutIntro, AboutDesign } from "../../content/About"
 import { Layout } from "../../layouts"
 
 export const AboutThisSite = () => {
+  const [aboutSectionToDisplay, setAboutSectionToDisplay] =
+    useState<string>("Intro")
+
   return (
     <Layout>
-      <AboutNav />
+      <AboutNav updateDisplay={setAboutSectionToDisplay} />
       <SiteSection>
         <div style={{ marginTop: "3.75rem", padding: "3rem" }}>
-          <AboutIntro />
+          {aboutSectionToDisplay === "Intro" && <AboutIntro />}
+          {aboutSectionToDisplay === "Design" && <AboutDesign />}
         </div>
       </SiteSection>
     </Layout>
